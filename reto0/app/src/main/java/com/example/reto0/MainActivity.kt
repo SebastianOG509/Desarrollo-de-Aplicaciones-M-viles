c9e9e21b93aaf64bd7e014e5e4cec0c051662c48
SebastianOG509 <seortizg@unal.edu.co>
2025-09-01 22:43:43 -0500

    reto 0

--- reto0/app/src/main/java/com/example/reto0/MainActivity.kt
+++ reto0/app/src/main/java/com/example/reto0/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.reto0.ui.theme.Reto0Theme
@@ -32,10 +34,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hola $name!",
-        modifier = modifier
-    )
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Hola $name!")
+    }
 }
 
 @Preview(showBackground = true)
